f201bd575dbe8891ca72704e8217c26ee45aebf1
Brian Arrieta <barrieta950@gmail.com>
2024-05-04 19:05:05 -0400

    brian hackaton

--- atlas-proyect/src/main.jsx
+++ atlas-proyect/src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { App } from './pages/App'
+import { CssBaseline } from '@mui/material';
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import { UserDetail } from './components/UserDetail';
+import { Form } from './pages/Form';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },{
+    path: '/error',
+    element: <div>error</div>
+  },{
+    path: "user/:userId",
+    element: <UserDetail />,
+  },{
+    path: "/form",
+    element: <Form />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
